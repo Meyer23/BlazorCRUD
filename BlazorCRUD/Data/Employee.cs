2947fae390e59a475d3af22b29e165dea2137fd5
Meyer23 <rmeyer93@fpuna.edu.py>
2022-03-29 21:07:06 -0400

    Agregar archivos de proyecto.

---
+++ BlazorCRUD/Data/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCRUD.Data
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string EmployeName { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Designation { get; set; }
+    }
+}
